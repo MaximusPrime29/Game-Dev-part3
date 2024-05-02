703bbeb7ddde5a71e79f037a89837ce69493ed84
Max Schulte <maxgs1001@gmail.com>
2024-05-02 12:49:13 +0200

    rough procedural generation

--- Assets/Scripts/Part 2 Scripts/BackgroundDestroy.cs
+++ Assets/Scripts/Part 2 Scripts/BackgroundDestroy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine;
 public class BackgroundDestroy : MonoBehaviour
 {
     GameObject[] player = null;
+    float playerZ;
+    float objectZ;
+    float zDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +19,17 @@ public class BackgroundDestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<Transform>().position.z < player[0].GetComponent<Transform>().position.z)
+        objectZ = GetComponent<Transform>().position.z;
+        playerZ = player[0].GetComponent<Transform>().position.z;
+        zDistance = objectZ + playerZ;
+        if (zDistance<- 30)
         {
-
             Destroy(gameObject);
+
         }
+
+
+        
             
 
     }
